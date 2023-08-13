01e2639462960cc4e82b2aefad323d45b6048c4a
Frederick Morrison <frederick.morrison@outlook.com>
2023-08-13 13:56:12 -0600

    add unscaled random walk

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import sys, ctypes
 import inspect
 import pyautogui
 import tkinter as tk
-
-# BAD: from matplotlib import pyplot as plt
-import matplotlib as mpl
+#
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy import random
-
+#
+from random_walk import RandomWalk
 
 def get_python_version() -> str:
     return f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
@@ -197,6 +196,39 @@ def display_multiple_plots():
 def display_random_walk():
     fn = f"\n{inspect.getframeinfo(inspect.currentframe()).function}"
     print(fn)
+    # Keep making new walks, as long as the program is active.
+    while True:
+        # Make a random walk.
+        rw = RandomWalk(50_000)
+        rw.fill_walk()
+    
+        # Plot the points in the walk.
+        plt.style.use("classic")
+        fig, ax = plt.subplots()
+        point_numbers = range(rw.num_points)
+        ax.scatter(
+            rw.x_values,
+            rw.y_values,
+            c=point_numbers,
+            cmap=plt.cm.Blues,
+            edgecolors="none",
+            s=1,
+        )
+        ax.set_aspect("equal")
+    
+        # Emphasize the first and last points.
+        ax.scatter(0, 0, c="green", edgecolors="none", s=100)
+        ax.scatter(rw.x_values[-1], rw.y_values[-1], c="red", edgecolors="none", s=100)
+    
+        # Remove the axes.
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+    
+        plt.show()
+    
+        keep_running = input("Make another walk? (y/n): ")
+        if keep_running == "n":
+            break
 
 
 def display_simple_numpy_plot():
