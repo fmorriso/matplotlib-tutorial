a97e96110e655ecf781e9c328f5c3e9f9c486944
Frederick Morrison <frederick.morrison@outlook.com>
2023-08-14 17:53:13 -0600

    add roll_two_dice

--- main.py
+++ main.py
@@ -377,6 +377,36 @@ def roll_die():
     # fig.print_grid()
     fig.show()
 
+def roll_two_dice():
+    fn = f"\n{inspect.getframeinfo(inspect.currentframe()).function}"
+    print(fn)
+    # create two 6-sided dice
+    die_1 = Die()
+    die_2 = Die()
+
+    # Make some rolls, and store results in a list.
+    results = []
+    for roll_num in range(50_000):
+        result = die_1.roll() + die_2.roll()
+        results.append(result)
+
+    # Analyze the results.
+    frequencies = []
+    max_result = die_1.num_sides + die_2.num_sides
+    poss_results = range(2, max_result + 1)
+    for value in poss_results:
+        frequency = results.count(value)
+        frequencies.append(frequency)
+
+    # Visualize the results.
+    title = f"{fn} - Results of Rolling a D6 and a D10 50,000 Times"
+    labels = {"x": "Result", "y": "Frequency of Result"}
+    fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+
+    # Further customize chart.
+    fig.update_layout(xaxis_dtick=1)
+
+    fig.show()
 
 def tutorial_random_dot_plot():
     fn = f"\n{inspect.getframeinfo(inspect.currentframe()).function}"
@@ -435,6 +465,7 @@ if __name__ == "__main__":
     # display_squares_scatter()
     # display_transposed_multi_line_plots()
     # display_w_plot()
-    roll_die()
+    # roll_die()
+    roll_two_dice()
     # tutorial_random_dot_plot()
     # tutorial_simple()
