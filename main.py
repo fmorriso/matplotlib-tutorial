724319a5391bf5e66af8bd3e7a96b4afaf8c3e57
Frederick Morrison <frederick.morrison@outlook.com>
2023-08-14 18:05:56 -0600

    add roll_two_unequal_sided_dice

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import ctypes
 import inspect
 import sys
 import tkinter as tk
+
 #
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,6 +12,7 @@ import pyautogui
 import pyautogui as pag
 from numpy import random
 import plotly.express as px
+
 #
 from random_walk import RandomWalk
 from die import Die
@@ -363,7 +365,7 @@ def roll_die():
         results.append(result)
     # print(results)
     frequencies = []
-    poss_results = range(1, die.num_sides+1) # instead of 0-to-5, we want 1-to-6
+    poss_results = range(1, die.num_sides + 1)  # instead of 0-to-5, we want 1-to-6
     for value in poss_results:
         frequency = results.count(value)
         frequencies.append(frequency)
@@ -371,11 +373,12 @@ def roll_die():
     # print(frequencies)
     # visualize
     title = f"{fn} - Results of Rolling One D6 1,000 Times"
-    labels = {'x': 'Result', 'y': 'Frequency of Result'}
+    labels = {"x": "Result", "y": "Frequency of Result"}
     # Reference: https://plotly.com/python-api-reference/generated/plotly.express.bar.html
     fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
     # fig.print_grid()
     fig.show()
+
 
 def roll_two_dice():
     fn = f"\n{inspect.getframeinfo(inspect.currentframe()).function}"
@@ -408,6 +411,41 @@ def roll_two_dice():
     fig.update_layout(xaxis_dtick=1)
 
     fig.show()
+
+
+def roll_two_unequal_sided_dice():
+    fn = f"\n{inspect.getframeinfo(inspect.currentframe()).function}"
+    print(fn)
+
+    # create two dice of different sizes
+    die_1 = Die()
+    die_2 = Die(10)
+
+    # Make some rolls, and store results in a list.
+    results = []
+    max_range = 50_000
+    for roll_num in range(max_range):
+        result = die_1.roll() + die_2.roll()
+        results.append(result)
+
+    # Analyze the results.
+    frequencies = []
+    max_result = die_1.num_sides + die_2.num_sides
+    poss_results = range(2, max_result + 1)
+    for value in poss_results:
+        frequency = results.count(value)
+        frequencies.append(frequency)
+
+    # Visualize the results.
+    title = f"{fn} - Results of Rolling a six-sided die and a ten-sided die {max_range:,} Times"
+    labels = {"x": "Result", "y": "Frequency of Result"}
+    fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+
+    # Further customize chart.
+    fig.update_layout(xaxis_dtick=1)
+
+    fig.show()
+
 
 def tutorial_random_dot_plot():
     fn = f"\n{inspect.getframeinfo(inspect.currentframe()).function}"
@@ -467,6 +505,7 @@ if __name__ == "__main__":
     # display_transposed_multi_line_plots()
     # display_w_plot()
     # roll_die()
-    roll_two_dice()
+    # roll_two_dice()
+    roll_two_unequal_sided_dice()
     # tutorial_random_dot_plot()
     # tutorial_simple()
